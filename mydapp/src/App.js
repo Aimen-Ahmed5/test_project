a1665b15b303df323742abf82ce9ccf0745de807
Aimen-Ahmed5 <aimenahmed63@gmail.com>
2022-02-07 19:46:57 +0500

    connect metamask automatically to browser

--- mydapp/src/App.js
+++ mydapp/src/App.js
@@ -11,8 +11,16 @@ function App() {
     // loadProvider is an async function
     const loadProvider = async () => {
       // on installing metamask on project, it injects two objects on brower 1. web3 2. ethereum 
-      console.log(window.web3);
-      console.log(window.ethereum); 
+      // console.log(window.web3);
+      // console.log(window.ethereum); 
+
+        {/* web3 will act as bridge to communicate frontend with blockchain */}
+
+      let provider = null;
+      // check if metamask is being used and this will automatically connect metamask to brower when loaded
+      if(window.ethereum){
+        provider = window.ethereum;
+      } 
     };
 
     loadProvider(); 
@@ -28,15 +36,16 @@ function App() {
           <p class="card-text"> Account : 0x0000000000
             {/* Account : {account ? account : "not connected"} */}
           </p>
-          <button
-           // connect front end with metamask
+          {/* <button
+            connect front end with metamask manually
+
             type="button" class="btn btn-success"
              onClick={async () => { 
                const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
               console.log(accounts);
             }} >
             Connect to metamask
-           </button>
+           </button> */}
            &nbsp;
           <button type="button" class="btn btn-success ">
             Transfer
